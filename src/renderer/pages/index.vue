1592d93e102953e9b06cb03002f96f5bf1afd255
kirill <alaevka@gmail.com>
2019-12-28 12:29:46 +0300

    as

--- src/renderer/pages/index.vue
+++ src/renderer/pages/index.vue
@@ -11,7 +11,7 @@
     <div class="w-1/3 flex justify-center">
       <nuxt-link to="/gorod" exact>
         <div class="text-center">
-          <div><img class="w-40 h-40 lg:w-56 lg:h-56 xl:w-64 xl:h-64 object-cover rounded-full border-2 border-dashed border-gray-200" src="~/assets/img/gorod/ulici/lenina/lenina_1-5.jpg" /></div>
+          <div><img class="w-40 h-40 lg:w-56 lg:h-56 xl:w-64 xl:h-64 object-cover rounded-full border-2 border-dashed border-gray-200" src="~/assets/img/gorod/ulici/lenina/1.jpg" /></div>
           <div class="bg-gray-200 py-2 px-3 opacity-50 rounded-lg mt-2"><h2 class="opacity-100 font-bold">ГОРОД</h2></div>
         </div>
       </nuxt-link>
